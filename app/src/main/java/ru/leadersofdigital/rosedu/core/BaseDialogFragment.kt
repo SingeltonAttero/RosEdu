050a421d115ab81a53a2d92fdc5518291bb6a883
Evgeniy <andreeff.ea@yandex.ru>
2020-11-14 14:49:46 +0200

    fix dialog

--- app/src/main/java/ru/leadersofdigital/rosedu/core/BaseDialogFragment.kt
+++ app/src/main/java/ru/leadersofdigital/rosedu/core/BaseDialogFragment.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
@@ -18,22 +19,21 @@ abstract class BaseDialogFragment<ST, VM : BaseViewModel<ST>>() : DialogFragment
 
      @androidx.annotation.StyleRes open val  styleRes = R.style.DialogTheme
 
-    //override fun onCreateView(
-    //    inflater: LayoutInflater,
-    //    container: ViewGroup?,
-    //    savedInstanceState: Bundle?
-    //): View? {
-    //    return inflater.inflate(layoutRes, container, false)
-    //}
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val inflater = LayoutInflater.from(context)
-        val view: View = inflater.inflate(layoutRes, null)
-
-        return AlertDialog.Builder(requireContext(), styleRes)
-            .setView(view)
-            .create()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutRes, container, false)
     }
+
+    //override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    //    val inflater = LayoutInflater.from(context)
+    //    val view: View = inflater.inflate(layoutRes, null)
+    //    return AlertDialog.Builder(requireContext(), styleRes)
+    //        .setView(view)
+    //        .create()
+    //}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
